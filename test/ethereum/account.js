70fcb83e9ebfa83a62685e21ee2745d5241f4f5b
Joseph Werle <joseph.werle@gmail.com>
2018-05-17 19:11:58 -0400

    test(test/ethereum/account.js): Just use provider URI directly

--- test/ethereum/account.js
+++ test/ethereum/account.js
@@ -4,8 +4,7 @@ const { create } = require('../../ethereum/account')
 const test = require('ava')
 const Web3 = require('web3')
 
-const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545') // from truffle develop
-const web3 = new Web3(provider)
+const web3 = new Web3('http://127.0.0.1:9545')
 
 test("create({entropy})", async (t) => {
   await t.throws(create({web3, entropy: 0}), TypeError, "entropy too small")
