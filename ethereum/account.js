5cacd5b533a2b742bb9f357720000e8e1190495a
Joseph Werle <joseph.werle@gmail.com>
2018-05-21 15:50:59 -0400

    refactor(ethereum/account.js): More doc and error checks

--- ethereum/account.js
+++ ethereum/account.js
@@ -2,14 +2,32 @@
 
 const { entropy } = require('./entropy')
 
-async function create({web3, entropy: E} = {}) {
-  E = await entropy(E)
-  return web3.eth.accounts.create(E)
+/**
+ * Creates an Ethereum account with web3 specified by
+ * an optional entropy value.
+ * @public
+ * @param {Object} opts
+ * @param {Object} opts.web3
+ * @param {Number} opts.entropy
+ * @throws TypeError
+ * @return {Object}
+ */
+async function create(opts) {
+  if (!opts || 'object' != typeof opts) {
+    throw new TypeError("ethereum.account.create: Expecting object.")
+  } else if (!opts.web3 || 'object' != typeof opts.web3) {
+    throw new TypeError("ethereum.account.create: Expecting web3 to be an object.")
+  }
+
+  const { web3 } = opts
+  const account = web3.eth.accounts.create(await entropy(opts.entropy))
+  return account
 }
 
 async function load() {
 }
 
 module.exports = {
-  create, load
+  create,
+  load,
 }
