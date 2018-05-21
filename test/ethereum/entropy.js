f20a288610241343e243c271dbd97d78a588b198
Joseph Werle <joseph.werle@gmail.com>
2018-05-21 15:44:11 -0400

    test(test/): Clean up tests

--- test/ethereum/entropy.js
+++ test/ethereum/entropy.js
@@ -1,14 +1,14 @@
 'use strict'
 
-const ethereum = require('../../ethereum')
+const { entropy } = require('../../ethereum/entropy')
 const test = require('ava')
 
 test("ethereum.entropy(size)", async (t) => {
   t.plan(6)
-  await t.throws(ethereum.entropy(0), TypeError, "too small")
-  await t.throws(ethereum.entropy(1), TypeError, "too small")
-  await t.throws(ethereum.entropy(-1), TypeError, "too small")
-  await t.throws(ethereum.entropy(15), TypeError, "too small")
-  void t.true(32 == (await ethereum.entropy(16)).length) // minimum
-  void t.true(64 == (await ethereum.entropy()).length) // default
+  await t.throws(entropy(0), TypeError, "too small")
+  await t.throws(entropy(1), TypeError, "too small")
+  await t.throws(entropy(-1), TypeError, "too small")
+  await t.throws(entropy(15), TypeError, "too small")
+  void t.true(32 == (await entropy(16)).length) // minimum
+  void t.true(64 == (await entropy()).length) // default
 })
