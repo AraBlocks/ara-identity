b0b1bcf95941b94e0aab770b8792ece2f8fe72c4
Joseph Werle <joseph.werle@gmail.com>
2018-05-21 15:51:24 -0400

    refactor(ethereum/wallet.js): Clean up wallet loading from account object

---
+++ ethereum/wallet.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { ethHexToBuffer } = require('../util')
+const { fromPrivateKey } = require('ethereumjs-wallet')
+
+async function load(opts) {
+  if (!opts || 'object' != typeof opts) {
+    throw new TypeError("ethereum.wallet.load: Expecting object.")
+  } else if (!opts.account || 'object' != typeof opts.account) {
+    throw new TypeError("ethereum.wallet.load: Expecting account to be an object.")
+  }
+
+  const { account } = opts
+
+  if (account.privateKey) {
+    const privateKey = ethHexToBuffer(account.privateKey)
+    return fromPrivateKey(privateKey)
+  }
+
+  return null
+}
+
+module.exports = {
+  load
+}
