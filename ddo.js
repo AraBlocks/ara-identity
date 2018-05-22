07dea5e483e5977ed37ea43316128c7fe5e69861
Joseph Werle <joseph.werle@gmail.com>
2018-05-22 17:13:13 -0400

    feat(ddo.js): Add initial ddo.js implementation

---
+++ ddo.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { DIDDocument } = require('did-document')
+
+/**
+ * Creates a DIDDocument from a DID for a DID.
+ * @public
+ * @param {Object} opts
+ * @param {Object} opts.id
+ * @return {DIDDocument}
+ * @throws TypeError
+ */
+function create(opts) {
+  if (null == opts) {
+    throw new TypeError("ara-identifier.ddo.create: Expecting object.")
+  }
+
+  if (null == opts.id) {
+    throw new TypeError("ara-identifier.ddo.create: Expecting identifier.")
+  }
+
+  return new DIDDocument({id})
+}
+
+module.exports = {
+  create
+}
