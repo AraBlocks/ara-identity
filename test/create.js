1be8bfd9402e410ff26797dd56726a1f21a9483d
Joseph Werle <joseph.werle@gmail.com>
2018-05-22 17:22:01 -0400

    test(test/create.js): Initial create tests stub

---
+++ test/create.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const { create } = require('../create')
+const context = require('ara-context')()
+const test = require('ava')
+
+test("create(opts)", async (t) => {
+  // TODO(jwerle): More tests
+  t.true('object' == typeof await create({context, password: 'password'}))
+})
