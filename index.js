bf5cb37b79e921dd2d2fb399eea6311d99c25c3b
Joseph Werle <joseph.werle@gmail.com>
2018-05-22 17:20:53 -0400

    refactor(index.js): Export keyPair, create, util, did, ddo

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 'use strict'
 
 const { resolve } = require('./resolve')
+const { create } = require('./create')
 const ethereum = require('./ethereum')
+const keyPair = require('./key-pair')
+const util = require('./util')
+const ddo = require('./ddo')
+const did = require('./did')
 
 module.exports = {
   ethereum,
+  keyPair,
   resolve,
+  create,
+  util,
+  ddo,
+  did,
 }
