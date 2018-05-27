37a099f521c394e5bf45364c2291bdbc8f9f930e
Joseph Werle <joseph.werle@gmail.com>
2018-05-27 13:58:06 -0400

    test(test/create.js): Read keystore conf in test

--- test/create.js
+++ test/create.js
@@ -2,12 +2,21 @@
 
 const { create } = require('../create')
 const context = require('ara-context')()
+const path = require('path')
+const pify = require('pify')
 const test = require('ava')
+const fs = require('fs')
 
 test("create(opts)", async (t) => {
   // TODO(jwerle): More tests
+  // @TODO(jwerle): Remove this
+  const { keystore } = JSON.parse(await pify(fs.readFile)(
+    path.resolve(__dirname, '../keystore.pub')
+  ))
+
+  const key = Buffer.alloc(16).fill('test')
   t.true('object' == typeof await create({
     context, password: 'password',
-    archive: { key: Buffer.from('hello') }
+    archive: { key, keystore }
   }))
 })
