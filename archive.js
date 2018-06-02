9119fc0883ab514e363482d2b6899b531a6a8cce
Joseph Werle <joseph.werle@gmail.com>
2018-06-02 19:42:23 -0400

    refactor(archive.js): Add retry support

--- archive.js
+++ archive.js
@@ -16,12 +16,30 @@ async function archive(identity, opts) {
   }
 
   if (null == opts || 'object' != typeof opts) {
-    throw new TypeError("ara-opts.archiver.archive: Expecting options object.")
+    throw new TypeError("ara-identity.archive: Expecting options object.")
   }
 
   Object.assign(opts, { onidentifier, onstream, onkey, onend })
 
-  const { network } = await archiver.sync.connect(opts)
+  let retries = opts.retries || 3
+  const result = { network: null }
+  setTimeout(ontimeout, opts.timeout || 5000)
+  await connect()
+  return result
+
+  async function connect() {
+    const { network } = await archiver.sync.connect(opts)
+    result.network = network
+  }
+
+  function ontimeout() {
+    network.swarm.destroy()
+    if (retries-- > 0) {
+      connect()
+    } else {
+      throw new Error("Out of retries in timeout")
+    }
+  }
 
   function onidentifier(connection, info) {
     return identity.cfs.identifier
