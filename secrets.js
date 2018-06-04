9fe406eef26a69e59a7c6c02549d4859a2da08cb
Joseph Werle <joseph.werle@gmail.com>
2018-06-03 20:57:16 -0400

    feat(secrets.js): Add identity secrets encryption/decryption

---
+++ secrets.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const protobuf = require('./protobuf')
+const crypto = require('ara-crypto')
+
+function encrypt(keys, secretKey) {
+  const iv = crypto.randomBytes(16)
+  const key = secretKey.slice(0, 16)
+  const flat = keys.map(map)
+  const signature = crypto.blake2b(Buffer.concat(flat))
+  const buffer = protobuf.messages.Keys.encode({signature, keys})
+  return crypto.encrypt(buffer, {key, iv})
+  function map({publicKey, secretKey}) {
+    return Buffer.concat([publicKey, secretKey])
+  }
+}
+
+function decrypt(doc, secretKey) {
+  const buffer = crypto.decrypt(doc, {secretKey})
+  const keys = protobuf.messages.Keys.decode(buffer)
+  return keys
+}
+
+module.exports = {
+  encrypt,
+  decrypt,
+}
