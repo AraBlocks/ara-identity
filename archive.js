e155be88b66aa83a8476afd3f33405c6726f2e76
Joseph Werle <joseph.werle@gmail.com>
2018-06-03 20:58:11 -0400

    refactor(archive.js): Move CFS interaction to archive

--- archive.js
+++ archive.js
@@ -1,6 +1,10 @@
 'use strict'
 
+const { createCFS } = require('cfsnet/create')
+const { dirname } = require('path')
+const { toHex } = require('./util')
 const archiver = require('ara-identity-archiver')
+const ram = require('random-access-memory')
 
 /**
  * Archive an identity into the network with
@@ -19,42 +23,68 @@ async function archive(identity, opts) {
     throw new TypeError("ara-identity.archive: Expecting options object.")
   }
 
+  const { publicKey, secretKey, files } = identity
+
+  const cfs = await createCFS({
+    secretKey: secretKey,
+    storage: ram,
+    shallow: true,
+    key: publicKey,
+    id: toHex(publicKey),
+  })
+
+  for (const file of files) {
+    const dir = dirname(cfs.resolve(file.path))
+    await cfs.writeFile(file.path, file.buffer)
+  }
+
   Object.assign(opts, { onidentifier, onstream, onkey, onend })
 
   let retries = opts.retries || 3
   const result = { network: null }
-  setTimeout(ontimeout, opts.timeout || 5000)
+  let timeout = null
   await connect()
+  clearTimeout(timeout)
   return result
 
   async function connect() {
+    clearTimeout(timeout)
+    timeout = setTimeout(ontimeout, opts.timeout || 5000)
     const { network } = await archiver.sync.connect(opts)
+    clearTimeout(timeout)
     result.network = network
   }
 
   function ontimeout() {
-    network.swarm.destroy()
-    if (retries-- > 0) {
+    clearTimeout(timeout)
+    if (result.network) {
+      result.network.swarm.destroy()
+    }
+
+    if (--retries > 0) {
       connect()
     } else {
-      throw new Error("Out of retries in timeout")
+      throw new Error("Failed to contact peer to archive identity.")
     }
   }
 
   function onidentifier(connection, info) {
-    return identity.cfs.identifier
+    return cfs.identifier
   }
 
   function onstream(connection, info) {
-    return identity.cfs.replicate({ live: false })
+    return cfs.replicate({ live: false })
   }
 
   function onkey(connection, info) {
-    return identity.cfs.key
+    return cfs.key
   }
 
   function onend(connection, info) {
-    network.swarm.destroy()
+    clearTimeout(timeout)
+    if (result.network) {
+      result.network.swarm.destroy()
+    }
   }
 }
 
