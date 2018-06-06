d951f9eb59895855a02bca3898a75725e015144d
Joseph Werle <joseph.werle@gmail.com>
2018-06-06 18:42:54 -0400

    refactor(index.js): Remove key-pair.js from index.js

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const { archive } = require('./archive')
 const { resolve } = require('./resolve')
 const { create } = require('./create')
 const ethereum = require('./ethereum')
-const keyPair = require('./key-pair')
 const util = require('./util')
 const ddo = require('./ddo')
 const did = require('./did')
 
 module.exports = {
   ethereum,
-  keyPair,
   archive,
   resolve,
   create,
