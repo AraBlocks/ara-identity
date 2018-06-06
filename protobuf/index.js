60d46705bf3b7c907fc31e6aa6b2f017244db80a
Joseph Werle <joseph.werle@gmail.com>
2018-06-06 17:59:11 -0400

    refactor(protobuf/index.js): Export schema as constant

--- protobuf/index.js
+++ protobuf/index.js
@@ -1,7 +1,12 @@
 'use strict'
 
+const { resolve } = require('path')
 const messages = require('./messages')
+const fs = require('fs')
+
+const kProtocolBufferSchema = fs.readFileSync(resolve(__dirname, 'index.proto'))
 
 module.exports = {
-  messages
+  kProtocolBufferSchema,
+  messages,
 }
