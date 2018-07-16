1aca9181fe392ef8bc0cc38d215c7385b29bc060
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-07-16 18:07:37 -0400

    feat(list.js): Make list as a separate module and add it to the CLI

---
+++ test/list.js
@@ -0,0 +1,10 @@
+const { list } = require('../list')
+const test = require('ava')
+
+test('list()', async (t) => {
+  t.plan(3)
+  const identities = await list()
+  t.true(null !== identities)
+  t.true('object' === typeof identities)
+  await t.throws(() => list('prashanth'), TypeError, 'Specified folder does not exist')
+})
