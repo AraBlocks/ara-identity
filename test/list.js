9d88f95fb1ddd49ee115c012099c69f448cab697
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-07-17 16:26:39 -0400

    feat(list.js): Refactor code

--- test/list.js
+++ test/list.js
@@ -1,8 +1,12 @@
+const context = require('ara-context')()
+const { create } = require('../create')
 const { list } = require('../list')
+const util = require('../util')
 const test = require('ava')
 
 test('list()', async (t) => {
-  t.plan(3)
+  const identity = await create({ context, password: 'password123'})
+  util.writeIdentity(identity)
   const identities = await list()
   t.true(null !== identities)
   t.true('object' === typeof identities)
