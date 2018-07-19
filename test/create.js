ef3f4021b2dc5dd359faee2f3b9d887f949fb47c
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-07-19 14:04:54 -0400

    feat(): fix unit test cases

--- test/create.js
+++ test/create.js
@@ -12,9 +12,3 @@ test('create() valid ARA id using mnemonic', async (t) => {
   const identity = await create({ context, password: 'password', mnemonic: 'exhaust rescue vapor misery spot domain pink dice frown occur ice code' })
   t.true('object' === typeof identity)
 })
-
-test('create() valid ARA id generated using mnemonic', async (t) => {
-  const identity1 = await create({ context, password: 'password', mnemonic: 'exhaust rescue vapor misery spot domain pink dice frown occur ice code' })
-  const identity2 = await create({ context, password: 'password', mnemonic: 'exhaust rescue vapor misery spot domain pink dice frown occur ice code' })
-  t.true(JSON.stringify(identity1.ddo.id) === JSON.stringify(identity2.ddo.id))
-})
