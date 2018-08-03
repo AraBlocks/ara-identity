59a81f76947ae0fa285e1269095a0ad3fa2b708c
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-08-03 18:16:28 -0400

    Feat(ethereum/wallet.js): Fix import statement

--- ethereum/wallet.js
+++ ethereum/wallet.js
@@ -1,6 +1,16 @@
-const { fromMasterSeed } = require('ethereumjs-wallet/dist/hdkey')
 const isZeroBuffer = require('is-zero-buffer')
 const isBuffer = require('is-buffer')
+
+// attempt to load hdkey from ethereumjs
+// see: https://github.com/ethereumjs/ethereumjs-wallet/issues/64
+let hdkey = null
+try { hdkey = require('ethereumjs-wallet/dist/hdkey') } catch (err) {
+  try { hdkey = require('ethereumjs-wallet/hdkey') } catch (err) {
+    throw new Error('Failed to load ethereumjs-wallet/hdkey.')
+  }
+}
+
+const { fromMasterSeed } = hdkey
 
 async function load(opts) {
   if (!opts || 'object' !== typeof opts) {
