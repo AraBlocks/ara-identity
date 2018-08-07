983acfc58587a7abd6c104cfb812604c747bc12f
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-08-07 15:45:10 -0400

    Feat(ethereum/wallet.js): Remove links

--- ethereum/wallet.js
+++ ethereum/wallet.js
@@ -2,8 +2,6 @@ const isZeroBuffer = require('is-zero-buffer')
 const isBuffer = require('is-buffer')
 
 /* eslint-disable import/no-unresolved */
-// attempt to load hdkey from ethereumjs
-// see: https://github.com/ethereumjs/ethereumjs-wallet/issues/64
 let hdkey = null
 try { hdkey = require('ethereumjs-wallet/dist/hdkey') } catch (err) {
   try { hdkey = require('ethereumjs-wallet/hdkey') } catch (error) {
