358d6f5a3a41ce8922b69fd5aa9c81479cb75cf2
Joseph Werle <joseph.werle@gmail.com>
2018-08-13 21:41:21 -0400

    refactor(protobuf/): Define syntax explicitly

--- protobuf/index.js
+++ protobuf/index.js
@@ -2,7 +2,10 @@ const { resolve } = require('path')
 const messages = require('./messages')
 const fs = require('fs')
 
-const kProtocolBufferSchema = fs.readFileSync(resolve(__dirname, 'index.proto'))
+const kProtocolBufferSchema = fs.readFileSync(resolve(
+  __dirname,
+  'schema.proto'
+))
 
 module.exports = {
   kProtocolBufferSchema,
