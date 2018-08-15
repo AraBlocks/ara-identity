3e6175ee1ac508cf0e68e05aa9517387893074c8
Joseph Werle <joseph.werle@gmail.com>
2018-08-14 20:10:03 -0400

    refactor(scripts/package.sh): Build for OS for now

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,21 +6,21 @@ BUILD=${BUILD:-build/}
 TARGET=${TARGET:-bin/ara-identity}
 
 rm -rf $BUILD
-mkdir -p $BUILD/{macos,linux,win}
+mkdir -p $BUILD
 
 build() {
-  $PKG -t $1 -o $BUILD/$1/aid $TARGET \
-    | grep $CWD                       \
-    | grep '\.node'                   \
-    | while read path; do             \
-      echo "dep: " $(basename $path); \
-      cp $path $BUILD/$1;             \
+  printf "> build: %s\n" $TARGET
+  $PKG -o $BUILD/aid $TARGET            \
+    | grep $CWD                         \
+    | grep '\.node'                     \
+    | while read path; do               \
+      printf ">   dep: %s\n" $(basename $path); \
+      cp $path $BUILD;                  \
     done
 
   local rc=$?
-  echo "> build: $TARGET for $1"
   return $rc
 }
 
-(build 'linux') && (build 'macos') && (build 'win')
+build
 exit $?
