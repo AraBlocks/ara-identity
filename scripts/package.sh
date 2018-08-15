d75bf4273c7f145539906aecaacf12dcfd5b386f
Joseph Werle <joseph.werle@gmail.com>
2018-08-15 11:48:19 -0400

    refactor(scripts/package.sh): Use pkg in node_modules if not found

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,17 +5,21 @@ PKG=${PKG:-$(which pkg)}
 BUILD=${BUILD:-build/}
 TARGET=${TARGET:-bin/ara-identity}
 
+if [ -z "$PKG" ]; then
+  PKG="node_modules/.bin/pkg"
+fi
+
 rm -rf $BUILD
 mkdir -p $BUILD
 
 build() {
   printf "> build: %s\n" $TARGET
-  $PKG -o $BUILD/aid $TARGET            \
-    | grep $CWD                         \
-    | grep '\.node'                     \
-    | while read path; do               \
+  $PKG -o $BUILD/aid $TARGET                    \
+    | grep $CWD                                 \
+    | grep '\.node'                             \
+    | while read path; do                       \
       printf ">   dep: %s\n" $(basename $path); \
-      cp $path $BUILD;                  \
+      cp $path $BUILD;                          \
     done
 
   local rc=$?
