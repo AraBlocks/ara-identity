f720d05915ec7ee9a30a6a407f467c2c8e22af98
Joseph Werle <joseph.werle@gmail.com>
2018-08-22 12:00:36 -0400

    refactor(ethereum/account.js): Clean up account load function

--- ethereum/account.js
+++ ethereum/account.js
@@ -1,35 +1,34 @@
-const isBuffer = require('is-buffer')
-const { recover } = require('./keystore')
 const { toHex, toBuffer } = require('../util')
-const fs = require('fs')
+const { recover } = require('./keystore')
+const { resolve } = require('path')
+const isBuffer = require('is-buffer')
+const crypto = require('ara-crypto')
 const pify = require('pify')
 const rc = require('../rc')()
-const crypto = require('ara-crypto')
-
-const {
-  resolve,
-  parse,
-  basename,
-  dirname
-} = require('path')
+const fs = require('fs')
 
 /**
  * Creates an Ethereum account with web3 specified by
  * an optional entropy value.
+ *
  * @public
  * @param {Object} opts
  * @param {Object} opts.web3
  * @param {Number} opts.entropy
- * @throws TypeError
  * @return {Object}
+ * @throws TypeError
  */
 async function create(opts) {
   if (!opts || 'object' !== typeof opts) {
-    throw new TypeError('ethereum.account.create: Expecting object.')
-  } else if (!opts.web3 || 'object' !== typeof opts.web3) {
-    throw new TypeError('ethereum.account.create: Expecting web3 to be an object.')
-  } else if (!opts.privateKey || false === isBuffer(opts.privateKey)) {
-    throw new TypeError('ethereum.account.create: Expecting privateKey to be a buffer')
+    throw new TypeError('Expecting object.')
+  }
+
+  if (!opts.web3 || 'object' !== typeof opts.web3) {
+    throw new TypeError('Expecting web3 to be an object.')
+  }
+
+  if (!opts.privateKey || false === isBuffer(opts.privateKey)) {
+    throw new TypeError('Expecting privateKey to be a buffer')
   }
 
   const { web3 } = opts
@@ -41,13 +40,14 @@ async function create(opts) {
 /**
  * Loads an Ethereum account based on a publicKey
  * and password.
+ *
  * @public
  * @param {Object} opts
  * @param {Object} opts.web3
  * @param {Object} opts.publicKey
  * @param {Object} opts.password
- * @throws {Error} If ethPath doesn't exist
- * @returns {Object} Resolved Ethereum account object
+ * @returns {Object}
+ * @throws {TypeError}
  */
 async function load(opts) {
   if (!opts || 'object' !== typeof opts) {
@@ -72,15 +72,26 @@ async function load(opts) {
   const publicKey = toBuffer(opts.publicKey)
   const hash = toHex(crypto.blake2b(publicKey))
 
-  const { araKeystore, ethKeystore } = rc.network.identity
-  const araPath = _constructKeystorePath(araKeystore, hash)
-  const ethPath = _constructKeystorePath(ethKeystore, hash)
-
-  let keys
   let encryptedKeystore
+  let keys
+
+  const araKeystorePath = resolve(
+    rc.network.identity.root,
+    hash,
+    'keystore',
+    'ara'
+  )
+
+  const ethKeystorePath = resolve(
+    rc.network.identity.root,
+    hash,
+    'keystore',
+    'eth'
+  )
+
   try {
-    keys = await pify(fs.readFile)(araPath, 'utf8')
-    encryptedKeystore = await pify(fs.readFile)(ethPath, 'utf8')
+    keys = await pify(fs.readFile)(araKeystorePath, 'utf8')
+    encryptedKeystore = await pify(fs.readFile)(ethKeystorePath, 'utf8')
   } catch (err) {
     throw new Error(err)
   }
@@ -89,13 +100,6 @@ async function load(opts) {
   const privateKey = web3.utils.bytesToHex(buf)
 
   return web3.eth.accounts.privateKeyToAccount(privateKey)
-}
-
-function _constructKeystorePath(path, hash) {
-  const parsedPath = parse(path)
-  const keystoreBase = basename(parsedPath.dir)
-  parsedPath.dir = dirname(parsedPath.dir)
-  return resolve(parsedPath.dir, hash, keystoreBase, parsedPath.base)
 }
 
 module.exports = {
