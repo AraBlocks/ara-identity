a46a09c1ce451cf000d2889889667e26903eab9d
Joseph Werle <joseph.werle@gmail.com>
2018-08-22 11:59:46 -0400

    refactor(rc.js): Remove useless properties

--- rc.js
+++ rc.js
@@ -6,12 +6,6 @@ const defaults = () => ({
   network: {
     identity: {
       root: resolve(rc().data.root, 'identities'),
-      keystore: resolve(rc().data.root, 'identities', 'keystore'),
-      ethKeystore: resolve(rc().data.root, 'identities', 'keystore', 'eth'),
-      araKeystore: resolve(rc().data.root, 'identities', 'keystore', 'ara'),
-      archive: {
-        root: resolve(rc().data.root, 'identities', 'archive'),
-      }
     }
   }
 })
