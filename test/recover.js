1a6f54b5bed6bf8e57fbf05debf12ba4a292b811
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-09-06 13:58:54 -0400

    chore(recover.js): linting

--- test/recover.js
+++ test/recover.js
@@ -6,23 +6,24 @@ const test = require('ava')
 
 test('recover() invalid opts', async (t) => {
   await t.throws(recover(), TypeError, 'Expecting opts to be an object.')
-  await t.throws(recover({context}), TypeError, 'Expecting password for recovery.')
-  await t.throws(recover({context, password: 123}), TypeError, 'Expecting mnemonic for recovery.')
-  await t.throws(recover({context, password: 123, mnemonic: 1234}), TypeError, 'Expecting mnemonic to be a string.')
-  await t.throws(recover({context, password: 123, mnemonic: 'hello how'}), TypeError, 'Expecting a valid bip39 mnemonic for recovery.')
+  await t.throws(recover({ context }), TypeError, 'Expecting password for recovery.')
+  await t.throws(recover({ context, password: 123 }), TypeError, 'Expecting mnemonic for recovery.')
+  await t.throws(recover({ context, password: 123, mnemonic: 1234 }), TypeError, 'Expecting mnemonic to be a string.')
+  await t.throws(recover({ context, password: 123, mnemonic: 'hello how' }), TypeError, 'Expecting a valid bip39 mnemonic for recovery.')
 })
 
 test('recover() using valid mnemonic', async (t) => {
   const mnemonic = bip39.generateMnemonic()
-  const identity = await recover({ context, password: 'password' ,mnemonic})
+  const identity = await recover({ context, password: 'password', mnemonic })
   t.true('object' === typeof identity)
   t.true(mnemonic === identity.mnemonic)
 })
 
 test('recover() compare recovered identity', async (t) => {
-  const identity = await create({context, password: '123'})
+  const identity = await create({ context, password: '123' })
+  // eslint-disable-next-line prefer-destructuring
   const mnemonic = identity.mnemonic
-  const recoveredID = await recover({context, password: 'hello', mnemonic})
+  const recoveredID = await recover({ context, password: 'hello', mnemonic })
   t.true(identity.account.privateKey === recoveredID.account.privateKey)
   t.true(identity.mnemonic === recoveredID.mnemonic)
   t.true(0 === Buffer.compare(identity.publicKey, recoveredID.publicKey))
