8d41efff634c80404d8eeb519c9719f66ca7c1d4
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-09-06 15:34:03 -0400

    feat(replicate.js): Initial Method Commit

---
+++ replicate.js
@@ -0,0 +1,18 @@
+/**
+ * Replicate & Download a DID's identity files from a remote server
+ * @public
+ * @param {String} identity
+ * @param {Object} opts
+ * @param {String} opts.keyring
+ * @param {String} opts.network
+ * @param {String} opts.secret
+ */
+
+async function replicate(identity, opts) {
+
+}
+
+
+module.exports = {
+  replicate
+}
