b7d120b336644a186f8c5786facf10b7db02ae92
Joseph Werle <joseph.werle@gmail.com>
2018-09-12 18:31:57 -0400

    refactor(ethereum/entropy.js): Clean up error messages

--- ethereum/entropy.js
+++ ethereum/entropy.js
@@ -8,7 +8,7 @@ async function entropy(size) {
   let entropySize = size
   if (null == entropySize) { entropySize = kDefaultEntropySize }
   if (!entropySize || entropySize < kMinEntropySize) {
-    throw new TypeError(`entropy: Invalid entropy size. Must be larger than ${kMinEntropySize}.`)
+    throw new TypeError(`Invalid entropy size. Must be larger than ${kMinEntropySize}.`)
   }
 
   return toHex(randombytes(entropySize))
