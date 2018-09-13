553b9544be5178d4fd9628f3485120f267cb6dab
Joseph Werle <joseph.werle@gmail.com>
2018-09-13 10:54:38 -0400

    refactor(did.js): Fix typos

--- did.js
+++ did.js
@@ -5,27 +5,27 @@ const { DID } = require('did-uri')
 const DID_ARA_METHOD = 'ara'
 
 /**
- * Creates a DID document (DDO) from an identitier.
+ * Creates a DID document (DDO) from an identifier.
  * @public
- * @param {String|Buffer} identitier
+ * @param {String|Buffer} identifier
  * @param {?(String)} [method]
  * @return {Object}
  * @throws TypeError
  */
-function create(identitier, method) {
-  if (!identitier) {
-    throw new TypeError('Expecting identitier.')
+function create(identifier, method) {
+  if (!identifier) {
+    throw new TypeError('Expecting identifier.')
   }
 
-  if ('string' !== typeof identitier && false === isBuffer(identitier)) {
-    throw new TypeError('Expecting identitier to be a string or buffer.')
+  if ('string' !== typeof identifier && false === isBuffer(identifier)) {
+    throw new TypeError('Expecting identifier to be a string or buffer.')
   }
 
   if (method && 'string' !== typeof method) {
     throw new TypeError('Expecting method to be a string.')
   }
 
-  const id = toHex(identitier)
+  const id = toHex(identifier)
   const didMethod = method || DID_ARA_METHOD
   const uri = `did:${didMethod}:${id}`
   return new DID(uri)
@@ -43,7 +43,7 @@ function normalize(uri, method) {
   }
 
   if ('string' !== typeof uri) {
-    throw new TypeError('Expecting uri to be a string or buffer.')
+    throw new TypeError('Expecting URI to be a string or buffer.')
   }
 
   if (method && 'string' !== typeof method) {
