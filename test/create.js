7a16ffcc1deeff5e913b9d428415e1789379719d
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-10-05 11:51:42 -0400

    chore(test/create.js): Add test case for mnemonic validation

--- test/create.js
+++ test/create.js
@@ -8,6 +8,14 @@ test('create() valid ARA id', async (t) => {
   t.true(null !== identity.mnemonic)
 })
 
+test('create() using invalid mnemonic', async (t) => {
+  await t.throws(
+    create({ context, password: 'password', mnemonic: 'exhaust' }),
+    TypeError,
+    'Expecting a valid bip39 mnemonic'
+  )
+})
+
 test('create() valid ARA id using mnemonic', async (t) => {
   const identity = await create({ context, password: 'password', mnemonic: 'exhaust rescue vapor misery spot domain pink dice frown occur ice code' })
   t.true('object' === typeof identity)
