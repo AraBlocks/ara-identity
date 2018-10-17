cb0709440dcdf07ec58e6ccc4429d2bd8fe86f5d
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-10-17 17:19:08 -0400

    chore(test/revoke.js): Add unit test cases for revoke()

---
+++ test/revoke.js
@@ -0,0 +1,72 @@
+const { resolve } = require('../resolve')
+const { create } = require('../create')
+const { revoke } = require('../revoke')
+const context = require('ara-context')()
+const bip39 = require('bip39')
+const util = require('../util')
+const test = require('ava')
+
+test('revoke() invalid opts', async (t) => {
+  t.plan(7)
+  const mnemonic = bip39.generateMnemonic()
+
+  await t.throws(
+    revoke(),
+    TypeError,
+    'Expecting opts to be an object.'
+  )
+
+  await t.throws(
+    revoke({ }),
+    TypeError,
+    'Expecting mnemonic for revoking.'
+  )
+
+  await t.throws(
+    revoke({ mnemonic: 1234 }),
+    TypeError,
+    'Expecting mnemonic to be a string.'
+  )
+
+  await t.throws(
+    revoke({ mnemonic }),
+    TypeError, 'Expecting password.'
+  )
+
+  await t.throws(
+    revoke({ mnemonic: 'hello' }),
+    TypeError,
+    'Expecting a valid bip39 mnemonic for revoking.'
+  )
+
+  await t.throws(
+    revoke({ mnemonic, password: 1234 }),
+    TypeError,
+    'Expecting password to be a string.'
+  )
+
+  await t.throws(
+    revoke({ mnemonic, password: 'test' }),
+    Error,
+    'Could not resolve DID for the provided mnemonic'
+  )
+})
+
+test('revoke(opts)', async (t) => {
+  t.plan(1)
+
+  const identity = await create({ context, password: 'test' })
+  await util.writeIdentity(identity)
+  const { mnemonic } = identity
+  const revokedIdentity = await revoke({ password: 'test2', mnemonic })
+  await util.writeIdentity(revokedIdentity)
+
+  let ddo
+  try {
+    ddo = await resolve(revokedIdentity.publicKey.toString('hex'))
+  } catch (err) {
+    throw new Error('Could not resolve DID for the provided mnemonic')
+  }
+
+  t.true('string' === typeof ddo.revoked)
+})
