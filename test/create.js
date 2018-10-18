abd67337a0837bb0c0c1eb3795a6e130d3c0ae6b
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-10-18 12:14:23 -0400

    chore(test/create.js): Add unit test case for service endpoint creation

--- test/create.js
+++ test/create.js
@@ -20,3 +20,24 @@ test('create() valid ARA id using mnemonic', async (t) => {
   const identity = await create({ context, password: 'password', mnemonic: 'exhaust rescue vapor misery spot domain pink dice frown occur ice code' })
   t.true('object' === typeof identity)
 })
+
+test('create() valid ARA id with service endpoints', async (t) => {
+  t.plan(2)
+  const service = []
+  service.push({
+    id: 'arasite',
+    type: 'ara-site.Service',
+    endpoint: 'http://www.ara.one',
+    description: 'This is our project site'
+  })
+  service.push({
+    id: 'aradev',
+    type: 'ara-dev.Service',
+    endpoint: 'http://www.ara.one/dev',
+    description: 'This is our developer page',
+    price: '10 Ara'
+  })
+  const identity = await create({ context, password: 'password', ddo: { service } })
+  t.true(true === Array.isArray(identity.ddo.service))
+  t.true(2 === identity.ddo.service.length)
+})
