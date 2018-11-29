aba3608c02532adead4ea73e1d0e2093d05fac75
Joseph Werle <joseph.werle@gmail.com>
2018-11-29 17:57:33 -0500

    chore(package.json): Add 'browserify' fields and 'brfs'

--- protobuf/index.js
+++ protobuf/index.js
@@ -2,7 +2,7 @@ const messages = require('./messages')
 const fs = require('fs')
 
 // eslint-disable-next-line prefer-template,no-path-concat
-const kProtocolBufferSchema = fs.readFileSync(__dirname + '/schema.proto')
+const kProtocolBufferSchema = fs.readFileSync(__dirname + '/schema.proto', 'utf8')
 
 module.exports = {
   kProtocolBufferSchema,
