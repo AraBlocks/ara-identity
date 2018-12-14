bfccfb6099c40a326bc39d07e24e96260729f22c
Joseph Werle <joseph.werle@gmail.com>
2018-12-14 12:45:18 -0500

    refactor(index.js): Export 'update()'

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { archive } = require('./archive')
 const { resolve } = require('./resolve')
 const { recover } = require('./recover')
 const { revoke } = require('./revoke')
+const { update } = require('./update')
 const { create } = require('./create')
 const { save } = require('./save')
 const ethereum = require('./ethereum')
@@ -20,6 +21,7 @@ module.exports = {
   resolve,
   revoke,
   create,
+  update,
   save,
   list,
   util,
