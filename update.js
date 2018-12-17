b282e636b5f3a33f9ff8d04d65cfbad5e849bd90
Joseph Werle <joseph.werle@gmail.com>
2018-12-17 16:08:33 -0500

    fix(update.js): Remove merge logic and enforce input DDO

--- update.js
+++ update.js
@@ -1,6 +1,5 @@
-const { resolve } = require('./resolve')
 const { create } = require('./create')
-const debug = require('debug')('ara:identity:update')
+// const debug = require('debug')('ara:identity:update')
 
 /**
  * Updates an ARA identity.
@@ -10,49 +9,12 @@ async function update(identifier, opts) {
     throw new TypeError('Expecting object.')
   }
 
-  let ddo = null
-
-  if (opts.ddo && opts.ddo.toJSON) {
-    opts.ddo = JSON.parse(JSON.stringify(opts.ddo.toJSON()))
-  }
-
-  try {
-    ddo = await resolve(identifier)
-
-    if (ddo.publicKey && opts.ddo.publicKey) {
-      append(ddo.publicKey, opts.ddo.publicKey)
-    } else if (ddo.publicKey) {
-      opts.ddo.publicKey = ddo.publicKey
-    }
-
-    if (ddo.authentication && opts.ddo.authentication) {
-      append(ddo.authentication, opts.ddo.authentication)
-    } else if (ddo.authentication) {
-      opts.ddo.authentication = ddo.authentication
-    }
-
-    if (ddo.service && opts.ddo.service) {
-      append(ddo.service, opts.ddo.service)
-    } else if (ddo.service) {
-      opts.ddo.service = ddo.service
-    }
-  } catch (err) {
-    debug(err)
-  }
-
-  if (ddo) {
-    opts.created = ddo.created
+  if (opts.ddo) {
+    opts.created = opts.ddo.created
     opts.updated = new Date()
-    opts.ddo = ddo
   }
 
   return create(opts)
-}
-
-function append(left, right) {
-  for (const x of right) {
-    left.push(x)
-  }
 }
 
 module.exports = {
