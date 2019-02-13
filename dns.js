5572b540f60ec6df868ed3bc6778f2cd7e6f81ab
Joseph Werle <joseph.werle@gmail.com>
2019-02-13 07:57:28 -0500

    feat(dns.js): Add 'dns.bind()' and 'dns.unbind()'

--- dns.js
+++ dns.js
@@ -1,19 +1,95 @@
-const { resolve } = require('ara-identity-dns')
+const isDomainName = require('is-domain-name')
+const { resolve } = require('./resolve')
+const { update } = require('./update')
+const { DID } = require('did-uri')
+const dns = require('ara-identity-dns')
+
+const RFC4501 = 'RFC4501'
+const DNS_SERVICE_TYPE = 'DNSURIRecord'
 
 async function query(domain, opts) {
-  const resolutions = await resolve(domain, opts)
+  const resolutions = await dns.resolve(domain, opts)
   return resolutions
 }
 
-async function bind(identity, domain, opts) {
-  // const ddo = await resolve(identity, opts)
-  void opts
-  console.warn('aid.dns.bind() is not implemented')
-  return false
+async function bind(identifier, domain, opts) {
+  if (!isDomainName(domain)) {
+    throw new TypeError(`Expecting '${domain}' to be a valid domain name.`)
+  }
+
+  const ddo = opts.ddo || await resolve(identifier, opts)
+  let isBound = false
+
+  for (const service of ddo.service) {
+    if (DNS_SERVICE_TYPE === service.type || RFC4501 === service.type) {
+      const p = service.id.split(';')
+      const { did } = new DID(p[0])
+      const name = p[1]
+      if (did === ddo.id && name === domain) {
+        isBound = true
+        break
+      }
+    }
+  }
+
+  if (isBound) {
+    throw new Error(`Domain '${domain}' is already bound to identity.`)
+  }
+
+  const authority = opts.authority ? `//${opts.authority}/` : ''
+
+  ddo.service.push({
+    id: `${ddo.id};${domain}`,
+    type: DNS_SERVICE_TYPE,
+    serviceEndpoint: `dns://${authority}${domain}?type=${opts.type || 'TXT'}`
+  })
+
+  return update(ddo.id, {
+    password: opts.password,
+    publicKey: opts.publicKey,
+    secretKey: opts.secretKey,
+    ddo,
+  })
+}
+
+async function unbind(identifier, domain, opts) {
+  if (!isDomainName(domain)) {
+    throw new TypeError(`Expecting '${domain}' to be a valid domain name.`)
+  }
+
+  const ddo = opts.ddo || await resolve(identifier, opts)
+  let wasBound = false
+
+  for (let i = 0; i < ddo.service.length; ++i) {
+    const service = ddo.service[i]
+    if (DNS_SERVICE_TYPE === service.type || RFC4501 === service.type) {
+      const p = service.id.split(';')
+      const { did } = new DID(p[0])
+      const name = p[1]
+
+      if (did === ddo.id && name === domain) {
+        ddo.service.splice(i, 1)
+        wasBound = true
+        break
+      }
+    }
+  }
+
+  if (wasBound) {
+    return update(ddo.id, {
+      password: opts.password,
+      publicKey: opts.publicKey,
+      secretKey: opts.secretKey,
+      ddo,
+    })
+  }
+
+  throw new Error(`Domain '${domain}' not bound to identity.`)
 }
 
 module.exports = {
   resolve,
+  unbind,
   query,
   bind,
 }
