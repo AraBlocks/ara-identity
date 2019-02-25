268b0325bd8357aee1a40c84e62aa8a8c4865dda
Joseph Werle <joseph.werle@gmail.com>
2019-02-25 16:55:06 -0500

    refactor(sign.js): Extract 'cache,network' options and parse DID first

--- sign.js
+++ sign.js
@@ -1,11 +1,19 @@
 const { resolve } = require('./resolve')
+const { DID } = require('did-uri')
 const crypto = require('ara-crypto')
 const ss = require('ara-secret-storage')
 const fs = require('./fs')
 
 async function sign(uri, message, opts) {
   const ddo = await resolve(uri, opts)
-  const keystore = JSON.parse(await fs.readFile(ddo.id, 'keystore/ara', opts))
+  const did = new DID(ddo.id)
+
+  const buffer = await fs.readFile(did.identifier, 'keystore/ara', {
+    cache: opts.cache,
+    network: opts.network,
+  })
+
+  const keystore = JSON.parse(buffer)
   const password = crypto.blake2b(Buffer.from(opts.password))
   const secretKey = ss.decrypt(keystore, { key: password.slice(0, 16) })
   return crypto.ed25519.sign(Buffer.from(message), secretKey)
